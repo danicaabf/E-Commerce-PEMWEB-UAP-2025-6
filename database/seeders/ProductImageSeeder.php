e2a5f2fb8f68f87c3819b14e413b6061e833d334
danicaabf <danica6002@gmail.com>
2025-12-09 15:46:11 +0700

    edit register&productimageseeder

--- database/seeders/ProductImageSeeder.php
+++ database/seeders/ProductImageSeeder.php
@@ -11,15 +11,15 @@ class ProductImageSeeder extends Seeder
     {
         $images = [
             [1, 'imagesource/nike-air-force-1.png'],
-            [2, 'imagesource/adidas-stan-smith.png'],
-            [3, 'imagesource/bata-oxford-classic.png'],
-            [4, 'imagesource/clarks-men-leather-formal.png'],
-            [5, 'imagesource/asics-gel-kayano.png'],
-            [6, 'imagesource/nike-pegasus-40.png'],
-            [7, 'imagesource/nike-lebron-witness.png'],
-            [8, 'imagesource/adidas-dame-8.png'],
-            [9, 'imagesource/dr-martens-1460.png'],
-            [10, 'imagesource/timberland-classic-boot.png'],
+            [2, 'imagesource/adidas-stan-smith.jpg'],
+            [3, 'imagesource/bata-oxford-classic.jpg'],
+            [4, 'imagesource/clarks-men-leather-formal.jpg'],
+            [5, 'imagesource/asics-gel-kayano.jpg'],
+            [6, 'imagesource/nike-pegasus-40.jpg'],
+            [7, 'imagesource/nike-lebron-witness.jpg'],
+            [8, 'imagesource/adidas-dame-8.jpg'],
+            [9, 'imagesource/dr-martens-1460.jpg'],
+            [10, 'imagesource/timberland-classic-boot.jpg'],
         ];
 
         foreach ($images as $img) {
